9899529f9482dabff084121f14c97b2aee5c9fe1
K-dev0425 <kante0425@outlook.com>
2022-04-06 13:28:28 +0800

    limit targetDna length to 16

--- zombieFeeding.sol
+++ zombieFeeding.sol
@@ -9,6 +9,6 @@ contract ZombieFeeding is ZombieFactory {
 
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
         require(msg.sender == zombieToOwner[_zombieId]);
-
+        _targetDna = _targetDna % dnaModulus;
     }
 }
